17cea3412429176703ec140ca4b966540ef38eba
Ulrich Anani <ulrichanan@gmail.com>
2022-09-19 03:45:43 +0100

    Update config

--- udagram-api-feed/src/controllers/v0/feed/models/FeedItem.ts
+++ udagram-api-feed/src/controllers/v0/feed/models/FeedItem.ts
@@ -2,7 +2,7 @@ import {Table, Column, Model, CreatedAt, UpdatedAt} from 'sequelize-typescript';
 
 
 @Table
-export class FeedItem extends Model<FeedItem> {
+export class FeedItem extends Model {
   @Column
   public caption!: string;
 
